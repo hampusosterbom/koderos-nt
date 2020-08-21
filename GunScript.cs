7e55d23bcf4dec49de2642baf7fa300aff603588
hampusosterbom <70038117+hampusosterbom@users.noreply.github.com>
2020-08-21 23:48:35 +0200

    Add files via upload

---
+++ GunScript.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Diagnostics;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+        void Shoot ()
+        {
+            RaycastHit hit;
+        Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range);
+            
+
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
