e78de83a5b0ba980e4e795c3b197da0046dd8d09
hampusosterbom <70038117+hampusosterbom@users.noreply.github.com>
2020-08-22 18:17:01 +0200

    Add files via upload

--- GunScript.cs
+++ GunScript.cs
@@ -7,26 +7,62 @@ public class GunScript : MonoBehaviour
 
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    AudioSource m_shootingSound;
+
+    private float nextTimeToFire = 0f;
+
+    void Start()
+    {
+        m_shootingSound = GetComponent<AudioSource>();
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
-    {
+                 {
         
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            m_shootingSound.Play();
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
 
     }
-        void Shoot ()
-        {
-            RaycastHit hit;
-        Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range);
-            
+       public void Shoot ()
+       {
+            muzzleFlash.Play();
 
-        }
+            RaycastHit hit;
+            if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range));
+            {
+              
+
+               Target target = hit.transform.GetComponent<Target>();
+               if (target != null)
+               { 
+                 target.TakeDamage(damage);
+               }
+
+               if(hit.rigidbody != null)
+               {
+                 hit.rigidbody.AddForce(-hit.normal * impactForce);
+               }
+
+               GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+               Destroy(impactGO, 2f);
+       
+       
+            }
+       }
 }
 
 
