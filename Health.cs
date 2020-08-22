1f81b9c814bd4429acb19318b4a1887e62e6a096
hampusosterbom <70038117+hampusosterbom@users.noreply.github.com>
2020-08-22 23:47:58 +0200

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 150;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+}
