2dff415a0a9ac3bd2a9f2c13a98e931498777f2d
hampusosterbom <70038117+hampusosterbom@users.noreply.github.com>
2020-08-23 19:34:10 +0200

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Media;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private const string PLAYER_ID_PREFIX = "Player";
+
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    public static void RegisterPlayer (string _netId, Player _player)
+    {
+        string _playerID = PLAYER_ID_PREFIX + _netId;
+        players.Add(_playerID, _player);
+        _player.transform.name = _playerID;
+    }
+
+
+   public static void UnRegisterPlayer (string _playerID)
+   {
+        players.Remove(_playerID);
+   }
+
+    void OnGUI ()
+    {
+        GUILayout.BeginArea(new Rect(200, 200, 200, 500));
+        GUILayout.BeginVertical();
+
+        foreach (string _playerID in players.Keys)
+        {
+            GUILayout.Label(_playerID + " " + players[_playerID].transform.name);
+        }
+
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+}
+
---
+++ PlayerShoot.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public Camera cam;
+    public PlayerWeapon weapon;
+    private const string PLAYER_TAG = "Player";
+
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+   
+    
+    void Update()
+    {
+
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot ()
+    {
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask)) ;
+        {
+            if(_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShoot(_hit.collider.name);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShoot (string _playerID)
+    {
+        
+    }
+}
